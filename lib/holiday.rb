6e4f3ce221a3abbd2feeaa16b8f0fcdcefbc6f5c
Sam Json <3115002443@mail2.gdut.edu.cn>
2018-12-14 08:06:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,6 +18,14 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash.each do |season,holiday|
+    holiday.each do |name,food|
+      if name == :fourth_of_july
+        return food[1]
+      end
+    end
+  end
+
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
@@ -61,10 +69,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
